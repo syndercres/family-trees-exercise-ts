e1f0ba0857d4546610e8b7a22c213dda4c2a9666
syndercombecreswell <syndercombecreswell@gmail.com>
2022-12-13 10:18:18 +0000

    creating trees

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,6 +1,27 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
+  
+    const eddardsKids: Person[] = [
+        { name: "Rickon", children: [] },
+        { name: "Bran", children: [] },
+        { name: "Arya", children: [] },
+        { name: "Sansa", children:[] },
+        { name: "Rob", children:[] },
+        { name: "Jon", children:[] },
+    ]
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+
+    const rickardsKids: Person[] = [
+        { name: "Benjen", children: [] },
+        { name: "Lyanna", children: [] },
+        { name: "Eddard", children: eddardsKids },
+        { name: "Brandon", children:[] },
+    ]
+
+  
+
+    const topPerson: Person = { name: "Rickard", children: rickardsKids };
+
+    return  topPerson ;
 }
